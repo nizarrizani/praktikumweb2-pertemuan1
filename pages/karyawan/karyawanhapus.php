328f49ffbf95df306137844fb7bd746724831db6
nizarrizani <xxnizarrizani@gmail.com>
2024-05-21 17:21:54 +0800

    3

---
+++ pages/karyawan/karyawanhapus.php
@@ -0,0 +1,39 @@
+<div id="top" class="row mb-3">
+    <div class="col">
+        <h3>Hapus Data karyawan</h3>
+    </div>
+    <div class="col">
+        <a href="?page=karyawan" class="btn btn-primary float-end">
+            <i class="fa fa-arrow-circle-left"></i>
+            Kembali
+        </a>
+    </div>
+</div>
+<div id="pesan" class="row mb-3">
+    <div class="col">
+        <?php
+        include "database/connection.php";
+        $nik = $_GET['nik'];
+
+        $sql = "DELETE FROM karyawan where nik = $nik";
+
+        $result = mysqli_query($connection, $sql);
+        if (!$result) {
+        ?>
+            <div class="alert alert-success" role="alert">
+                <i class="fa fa-exclamation-circle"></i>
+                <?php echo mysqli_error($connection) ?>
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class="alert alert-success" role="alert">
+                <i class="fab fa-check-circle"></i>
+                Hapus data berhasil
+            </div>
+            <meta http-equiv="refresh" content="5;url=?page=karyawan">
+        <?php
+        }
+        ?>
+    </div>
+</div>
